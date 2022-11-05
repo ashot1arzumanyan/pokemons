890e90cdd84923d72714491e7da76abb9df2b75f
Ashot Arzumanyan <aarzumanyan@moneylion.com>
2022-11-05 21:33:37 +0400

    fix loading bundle from nested path

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
   devtool: 'inline-source-map',
-  devServer: {
-    static: './build',
-  },
   module: {
     rules: [
       {
@@ -22,10 +19,18 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(process.cwd(), 'build'),
+    publicPath: '',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(process.cwd(), 'public/index.ejs')
-    })
-  ]
+      template: path.resolve(process.cwd(), 'public/index.ejs'),
+    }),
+  ],
+  devServer: {
+    static: './build',
+    port: 3000,
+    historyApiFallback: {
+      index: 'index.html'
+    },
+  },
 };
